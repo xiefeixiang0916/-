3df5cac801ca4ba5565927526aecc9154dff0937
xiefeixiang0916 <1547879034@qq.com>
2020-04-21 11:34:09 +0800

    基础文件

--- assets/js/token.js
+++
@@ -1,8 +0,0 @@
-// 如果登录了。则会有token保存。如果没有登录，没有token
-// console.log(typeof localStorage.getItem('token'))
-
-// 判断，如果没有token。就让跳转到登录页
-if (!localStorage.getItem('token')) {
-    // console.log(2222)
-    location.href = '/login.html';
-}
